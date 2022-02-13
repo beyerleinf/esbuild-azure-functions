82f15d06398fea4e275d012454b7ec28d28af3d3
Fabian Beyerlein <dev@beyerleinf.de>
2022-02-13 18:51:54 +0100

    chore(cli): Read package info

--- src/cli.ts
+++ src/cli.ts
@@ -4,12 +4,9 @@ import { build } from './lib/builder';
 import { promises as fs } from 'fs';
 
 const main = async () => {
-  const { version } = JSON.parse(await fs.readFile(`${__dirname}/../../package.json`, 'utf8'));
+  const { name, version, description } = JSON.parse(await fs.readFile(`${__dirname}/../../package.json`, 'utf8'));
 
-  program
-    .name('esbuild-azure-functions')
-    .description('A builder for Azure Functions powered by esbuild.')
-    .version(version, '-v, --version');
+  program.name(name).description(description).version(version, '-v, --version');
   program.requiredOption('-c, --config <file>', 'the config file to use', './esbuild-azure-functions.config.json');
   program.showSuggestionAfterError();
   program.parse();
